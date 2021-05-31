c48543c0cbafd90e990eb28d91f8fa87da97cd78
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-05-31 14:08:52 +0530

    Basic Project setup & running via tsc, node

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+let word:string;
+word = "hello world";
+console.log(word);
+
+interface User {
+  id: number,
+  name: string,
+}
+
+const person:User = {
+  id :3,
+  name : "raj"
+}
+
+const person2:User = {
+  id : 3,
+  name : "Rajesh"
+}
